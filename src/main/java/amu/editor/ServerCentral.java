aecadd0d34a40133bac69ef8870a37cba67c839e
b21228304 <douaa.benhila@etu.unniv-amu.fr>
2025-03-21 15:20:59 +0100

    Corriger classe ServerCentral.java

---
+++ src/main/java/amu/editor/ServerCentral.java
@@ -0,0 +1,35 @@
+package amu.editor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class ServerCentral {
+    private static List<String> document = new ArrayList<>(); // mon document partagé je le modelise avec une array list
+    private static int port = 1234;// j edefinis mon port
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Serveur démarré sur le port " + port);
+            while (true) {// j'attend la connexion du client
+                Socket socket = serverSocket.accept();// j'ai créer un serveur qui est capable d'accepter un client a n importe qulle moment donnée
+                System.out.println("Nouveau client connecté : " + socket.getInetAddress());
+                // chaque fois un client est connecté je crée pour luis on propre thread avec Editeur une classe que je vais créer ulterieurment
+                new Thread(new Editeur(socket, document)).start();
+
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
