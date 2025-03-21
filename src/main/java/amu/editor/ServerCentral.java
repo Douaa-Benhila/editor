47ee91568e9ff9bf2473cdf1a179a6a8a240bd65
b21228304 <douaa.benhila@etu.unniv-amu.fr>
2025-03-21 15:32:50 +0100

    succed part 1 of task 1

--- src/main/java/amu/editor/ServerCentral.java
+++ src/main/java/amu/editor/ServerCentral.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 
 
+
 public class ServerCentral {
     private static List<String> document = new ArrayList<>(); // mon document partagé je le modelise avec une array list
     private static int port = 1234;// j edefinis mon port
@@ -19,7 +20,7 @@ public class ServerCentral {
                 Socket socket = serverSocket.accept();// j'ai créer un serveur qui est capable d'accepter un client a n importe qulle moment donnée
                 System.out.println("Nouveau client connecté : " + socket.getInetAddress());
                 // chaque fois un client est connecté je crée pour luis on propre thread avec Editeur une classe que je vais créer ulterieurment
-                new Thread(new Editeur(socket, document)).start();
+                new Editeur(socket, document).run();
 
 
 
