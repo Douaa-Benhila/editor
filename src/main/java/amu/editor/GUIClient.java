c82e93225d051b411e41248b4bcf2bf2cdfae7f8
b21228304 <douaa.benhila@etu.unniv-amu.fr>
2025-03-25 16:45:07 +0100

    no change needed

--- src/main/java/amu/editor/GUIClient.java
+++ src/main/java/amu/editor/GUIClient.java
@@ -6,17 +6,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.net.URL;
 
 public class GUIClient extends javafx.application.Application{
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
     @Override
     public void start(Stage primaryStage) {
+
+
         URL url = getClass().getResource("clientView.fxml");
         System.out.println("URL: " + url);
         FXMLLoader loader = new FXMLLoader(getClass().getResource("clientView.fxml"));
         Parent root = null;
         try {
+
             root = loader.load();
         } catch (IOException e) {
             e.printStackTrace();
