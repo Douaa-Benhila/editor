48f1896e51a63aef0ceb770926902325759e08a5
b21228304 <douaa.benhila@etu.unniv-amu.fr>
2025-03-28 14:24:37 +0100

    add class ServerCentral.java

--- src/main/java/amu/editor/ServerCentral.java
+++ src/main/java/amu/editor/ServerCentral.java
@@ -5,31 +5,29 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-
-
+import java.util.concurrent.locks.ReentrantLock;
 
 
 public class ServerCentral {
-    private static List<String> document = new ArrayList<>(); // mon document partagé je le modelise avec une array list
-    private static int port = 1234;// j edefinis mon port
+    private static final int port = 12345; // je definis mon port
+    private final List<String> document = new ArrayList<>();//mon document partagé je le modelise avec  liste
+    private final ReentrantLock lock = new ReentrantLock();//assurer que l'accès à la ressource partagée se fait de manière sécurisée
 
     public static void main(String[] args) {
-        document.add("le projet editor");
-        document.add("réseaux");
+        new ServerCentral().startServer();
+    }
+
+    public void startServer() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Serveur démarré sur le port " + port);
-            while (true) {// j'attend la connexion du client
-                Socket socket = serverSocket.accept();// j'ai créer un serveur qui est capable d'accepter un client a n importe qulle moment donnée
-                System.out.println("Nouveau client connecté : " + socket.getInetAddress());
-                // chaque fois un client est connecté je crée pour luis on propre thread avec Editeur une classe que je vais créer ulterieurment
-                new Editeur(socket, document).run();
+            while (true) { // // j'attend la connexion du client
+                Socket clientSocket = serverSocket.accept();//j'ai créer un serveur qui est capable d'accepter un client a n'importe quelle moment donnée
+                // chaque fois un client est connecté je crée pour lui son propre thread avec ClientHandler une classe que je vais créer ulterieurment
+                new ClientHandler(clientSocket).start();
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
-
 
