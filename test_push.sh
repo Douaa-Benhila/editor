b099e6508f6ed27047f4578c19c83548582f207f
Badr <badr.motaim@etu.univ-amu.fr>
2025-04-11 22:53:22 +0200

    Adding the test_push.sh file

---
+++ test_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+CLASSPATH="build/classes/java/main"
+
+# commande avec lequel je lance mon script
+xterm -hold -e "java -cp $CLASSPATH amu.editor.ServerCentralPush" &
+
+# j'attends que le serveur demare
+sleep 2
+
+# modification de mes clients
+xterm -hold -e "java -cp $CLASSPATH amu.editor.AutoClientPush Client1 \"ADDL 1 Ligne A\" \"MDFL 0 Modif A\"" &
+xterm -hold -e "java -cp $CLASSPATH amu.editor.AutoClientPush Client2 \"ADDL 2 Ligne B\" \"RMVL 3\"" &
+xterm -hold -e "java -cp $CLASSPATH amu.editor.AutoClientPush Client3 \"MDFL 1 Final B\"" &
+
+# j'attends que tous les clients termines la modifications
+wait
+
